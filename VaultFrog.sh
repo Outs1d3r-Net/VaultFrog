8b5c5b848ff0a6f76da4a7f3105dc9de5efaf248
Outs1d3r-Net <41843160+Outs1d3r-Net@users.noreply.github.com>
2022-09-16 04:57:11 -0300

    Add various methods. Guard, List and Update.

--- VaultFrog.sh
+++ VaultFrog.sh
@@ -2,26 +2,54 @@
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ];then
 
-    #Help
-    echo -e "[*] Vault Frog Help [*]\n\n\t--view-pass SITE USER\t# For get cred.\n\t--help or -h\t\t# Show this message\n\t--list or -l\t\t# Show creds store.\n\t/bin/bash $0\t# For start vaultfrog.\n\n[*]"
+    # Help
+    echo -e "[*] Vault Frog Help [*]\n\n\t--view-pass SITE USER\t# For get cred.\n\t--help or -h\t\t# Show this message\n\t--list or -l\t\t# Show creds store.\n\t--update ID USERNAME NEW_PASSWORD # Update cred password.\t\n\t--guard SITE_NAME USER_NAME PASSWORD # For add creds.\t\t\n\t/bin/bash $0\t# For start vaultfrog.\n\n[*]"
     exit 0;
 
 elif [ "$1" == "--list" ] || [ "$1" == "-l" ];then
 
-    sqlite3 $HOME/.vaultfrog/.creds.db "select site,userN from secrets;" | column -s "|" -t
+    # List method.
+    sqlite3 $HOME/.vaultfrog/.creds.db "SELECT id,site,userN FROM secrets;" | column -s "|" -t
+    exit 0;
+
+elif [ "$1" == "--update" ];then
+
+    # Update method.
+    fEnc="/tmp/.vaultfrog$RANDOM.f"
+    echo $4 >> $fEnc
+    passEnc="$(openssl enc -aes-256-cbc -a -salt -in $fEnc)"
+    sqlite3 $HOME/.vaultfrog/.creds.db "UPDATE secrets SET pass='$passEnc' WHERE id='$2' AND userN='$3'"
+
+    # Clear.
+    echo "croack!" > $fEnc
+    rm -rf $fEnc
+    exit 0;
+
+elif [ "$1" == "--guard" ];then
+
+    # Add creds method.
+    fEnc="/tmp/.vaultfrog$RANDOM.f"
+    echo $4 >> $fEnc
+    passEnc="$(openssl enc -aes-256-cbc -a -salt -in $fEnc)"
+    sqlite3 $HOME/.vaultfrog/.creds.db "INSERT INTO secrets (site,userN,pass) VALUES ('$2','$3','$passEnc');"
+    echo "Credential $2:$3 add successful."
+
+    # Clear.
+    echo "croack!" > $fEnc
+    rm -rf $fEnc
     exit 0;
 
 elif [ "$1" == "--view-pass" ];then
 
-    #Get Encrypt Pass.
-    passR="$(sqlite3 $HOME/.vaultfrog/.creds.db "select pass from secrets where site like '%$2%' and userN='$3';")"
+    # Get Encrypt Pass.
+    passR="$(sqlite3 $HOME/.vaultfrog/.creds.db "select pass FROM secrets WHERE site LIKE '%$2%' AND userN='$3';")"
 
-    #Decrypt pass.
+    # Decrypt pass.
     fEnc="/tmp/.vaultfrog$RANDOM.f"
     echo $passR >> $fEnc
     openssl enc -d -aes-256-cbc -a -salt -in $fEnc
 
-    #Clear.
+    # Clear.
     echo "croack!" > $fEnc
     rm -rf $fEnc
     exit 0;
@@ -57,11 +85,11 @@ else
 
     # Create or Store data.
     if [ -f $HOME/.vaultfrog/.creds.db ];then
-        sqlite3 $HOME/.vaultfrog/.creds.db "insert into secrets (site,userN,pass) values ('$site','$userN','$passEnc');"
+        sqlite3 $HOME/.vaultfrog/.creds.db "INSERT INTO secrets (site,userN,pass) VALUES ('$site','$userN','$passEnc');"
     else
         mkdir -p $HOME/.vaultfrog/
-        sqlite3 $HOME/.vaultfrog/.creds.db "create table secrets (id INTEGER PRIMARY KEY,site TEXT,userN TEXT,pass TEXT);"
-        sqlite3 $HOME/.vaultfrog/.creds.db "insert into secrets (site,userN,pass) values ('$site','$userN','$passEnc');"
+        sqlite3 $HOME/.vaultfrog/.creds.db "CREATE TABLE secrets (id INTEGER PRIMARY KEY,site TEXT,userN TEXT,pass TEXT);"
+        sqlite3 $HOME/.vaultfrog/.creds.db "INSERT INTO secrets (site,userN,pass) VALUES ('$site','$userN','$passEnc');"
     fi
 
     # Show passwords.
